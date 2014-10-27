64da60110a5880a83ce6560a0c15035bb1d84af6
Melanie Sedda <melaniesedda@gmail.com>
2014-10-27 11:05:22 +0100

    ntohs

--- receiver.c
+++ receiver.c
@@ -153,14 +153,20 @@ int main (int argc, char **argv) {
       fprintf(stderr, "getnameinfo: %s\n", gai_strerror(s));
     
     /* If the CRC is not correct, the packet is dropped */
+<<<<<<< Updated upstream
     len = *payload_len;
     uint32_t expected_crc = rc_crc32((struct packet*) packet);
+=======
+    len = ntohs(*payload_len);
+    uint32_t expected_crc = rc_crc32(0, packet, 4 + PAYLOAD_SIZE);
+>>>>>>> Stashed changes
     if (*crc == expected_crc && len <= PAYLOAD_SIZE) {
       
-      /* Sanity check : the receiver only receives DATA packets with a receiving window size equal to zero */
+      /* Sanity check : the receiver only receives DATA packets of size <= 512 bytes with
+       a receiving window size equal to zero */
       type = (*header >> WINDOW_SIZE);
       window = (*header & BUFFER_SIZE);
-      if (type == PTYPE_DATA && window==0) {
+      if (type == PTYPE_DATA && window==0 && len<=512) {
         
         /* A packet outside the receiving window is dropped */
         if (*seq_num >= ((lastack+1) %N) && *seq_num <= ((lastack+real_window_size) %N)) {
